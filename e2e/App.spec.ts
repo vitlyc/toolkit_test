ef0205f25503fdf6bf24d7f7eb7d046405e296dd
Vitaly <72764133+vitlyc@users.noreply.github.com>
2023-05-27 19:18:46 +0300

    refac

--- e2e/App.spec.ts
+++ e2e/App.spec.ts
@@ -8,51 +8,52 @@ test.use({
 })
 
 test("test", async ({ page }) => {
-  test.setTimeout(60000)
   await page.goto("http://localhost:5173/")
   await page.getByRole("textbox").click()
   await page.getByRole("textbox").fill("typescript")
+
   await page.getByRole("button", { name: "Search" }).click()
   await page.waitForTimeout(2000)
 
-  await page.getByRole("link", { name: "3" }).click()
-  await page.waitForTimeout(2000)
-
-  await page.getByRole("link", { name: "4" }).click()
-  await page.waitForTimeout(2000)
-
-  await page.getByRole("link", { name: "5" }).click()
-  await page.waitForTimeout(2000)
-
-  await page.getByRole("link", { name: "8" }).click()
-  await page.waitForTimeout(2000)
-
-  await page.getByRole("link", { name: "1", exact: true }).click()
-  await page.waitForTimeout(2000)
-
-  await page.getByRole("textbox").click()
-  await page.waitForTimeout(2000)
-
   await page
-    .getByText("Name: typescriptStars: 113Last commit: 2023-05-15T09:11:04Z")
+    .getByText("Name: typescriptStars: 539Last commit: 2023-05-27T14:06:34Z")
     .click()
   await page.waitForTimeout(2000)
 
   await page.getByRole("button", { name: "Back" }).click()
   await page.waitForTimeout(2000)
 
+  await page.getByText("Name: typescript-book").click()
+  await page.waitForTimeout(2000)
+
+  await page.getByRole("button", { name: "Back" }).click()
+  await page.waitForTimeout(2000)
+
+  await page
+    .getByText("Name: TypeScriptStars: 91527Last commit: 2023-05-27T15:02:08Z")
+    .click()
+  await page.waitForTimeout(2000)
+
+  await page.getByRole("button", { name: "Back" }).click()
+  await page.waitForTimeout(2000)
+
+  await page.getByRole("button", { name: "10" }).click()
+
+  await page.getByRole("button", { name: "9" }).click()
+  await page.waitForTimeout(2000)
+
+  await page.getByRole("button", { name: "8" }).click()
+  await page.waitForTimeout(2000)
+
+  await page.getByRole("button", { name: "6" }).click()
+  await page.waitForTimeout(2000)
+
   await page
     .getByText(
-      "Name: vue-typescript-admin-templateStars: 5156Last commit: 2023-05-26T09:58:19Z"
+      "Name: awesome-typescript-loaderStars: 2357Last commit: 2023-05-01T03:38:57Z"
     )
     .click()
   await page.waitForTimeout(2000)
 
   await page.getByRole("button", { name: "Back" }).click()
-  await page.getByRole("textbox").click()
-  await page.getByRole("textbox").fill("banana")
-  await page.waitForTimeout(2000)
-
-  await page.getByRole("button", { name: "Search" }).click()
-  await page.waitForTimeout(2000)
 })
